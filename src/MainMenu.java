fa77de60d83c65fa1508f552f8127909579d4e3c
344813928 <344813928@C-87YLND2.tdsb.on.ca>
2026-05-15 09:59:25 -0400

    jframes

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -129,13 +129,14 @@ public class MainMenu extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        new Scenario1().setVisible(true); // open next scenario frame
+        this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
